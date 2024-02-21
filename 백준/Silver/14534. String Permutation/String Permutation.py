45daf5840462a04c2dd38f5e02b2afe4e3809dee
LEESUNGWOOK <40846999+gbk1414@users.noreply.github.com>
2024-02-21 09:57:54 +0900

    [Silver V] Title: String Permutation, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14534. String Permutation/String Permutation.py
+++ 백준/Silver/14534. String Permutation/String Permutation.py
@@ -1,25 +1,23 @@
-def str_permute(L_str):
-    if len(L_str) == 1:
-        return L_str
-    
-    L_str_list = []
+def str_permute(input_str):
+    if len(input_str) == 1:
+        return input_str
 
-    for i in range(len(L_str)):
-        new_L_str = L_str[:i]+L_str[i+1:]
-        for permute_L in str_permute(new_L_str):
-            L_str_list.append(L_str[i]+permute_L)
+    perm_str = []
+    for i in range(len(input_str)):
+        for permute_L in str_permute(input_str[:i]+input_str[i+1:]):
+            perm_str.append(input_str[i]+permute_L)
 
-    return L_str_list
+    return perm_str
 
 
 def main():
     T = int(input())
-    for i in range(1,T+1):
+    for i in range(1, T+1):
         print("Case # {}:".format(i))
         L = list(input())
         for s in str_permute(L):
             print(s)
 
+
 if __name__ == "__main__":
     main()
-    
