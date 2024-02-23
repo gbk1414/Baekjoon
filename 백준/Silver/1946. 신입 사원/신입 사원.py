493c4eefbe2e14e110b89df32e6dde791cadc1ba
LEESUNGWOOK <40846999+gbk1414@users.noreply.github.com>
2024-02-23 17:15:10 +0900

    [Silver I] Title: 신입 사원, Time: 2940 ms, Memory: 58552 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -1,27 +1,25 @@
-# 서류 1등의 면접등수보다 같거나 높은 and 면접 1등의 서류점수 보다 같거나 높은사람
-
 import sys
 
 input = sys.stdin.readline
 
 
-def get_new_worker(scores: list[int]) -> int:
+def count_new_workers(scores: list[tuple[int, int]]) -> int:
     scores.sort()
-    cnt = 1
+    count = 1
     min_score = scores[0][1]
-    for x,y in scores:
-        if min_score > y:
-            cnt += 1 
-            min_score = y
-    return cnt
+    for _, interview_score in scores:
+        if min_score > interview_score:
+            count += 1
+            min_score = interview_score
+    return count
 
 
 def main():
     T = int(input())
     for _ in range(T):
         N = int(input())
-        scores = [list(map(int, input().split())) for _ in range(N)]
-        print(get_new_worker(scores))
+        scores = [tuple(map(int, input().split())) for _ in range(N)]
+        print(count_new_workers(scores))
 
 
 if __name__ == "__main__":
