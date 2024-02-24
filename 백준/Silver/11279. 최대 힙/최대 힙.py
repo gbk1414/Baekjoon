a922c84d1dd0838141bff17a5715b4b865cdb016
LEESUNGWOOK <40846999+gbk1414@users.noreply.github.com>
2024-02-24 10:46:07 +0900

    [Silver II] Title: 최대 힙, Time: 112 ms, Memory: 38068 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.py
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -1,10 +1,7 @@
 import heapq 
 import sys
 
-N = int(sys.stdin.readline())
-h = []
-
-def update_heap(x: int) -> None:
+def update_heap(h: list[int], x: int) -> None:
     if x == 0:
         if h:
             heap_val = (-1) * heapq.heappop(h)
@@ -12,8 +9,15 @@ def update_heap(x: int) -> None:
         else:
             print(0)
     else:
-        heapq.heappush(h,(-1)*x)
+        heapq.heappush(h, (-1) * x)
 
-for _ in range(N):
-    x = int(sys.stdin.readline())
-    update_heap(x)
+def main():
+    N = int(sys.stdin.readline())
+    h = []
+
+    for _ in range(N):
+        x = int(sys.stdin.readline())
+        update_heap(h, x)
+
+if __name__ == "__main__":
+    main()
