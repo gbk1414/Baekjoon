3d8ea0034229704ac5c1bf1b42e7fe420bdcc07e
LEESUNGWOOK <40846999+gbk1414@users.noreply.github.com>
2024-02-24 15:20:40 +0900

    [Silver II] Title: 예산, Time: 72 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Silver/2512. 예산/예산.py
@@ -0,0 +1,27 @@
+def maximum_budget(nums: list[int], M: int) -> int:
+    nums.sort()
+    start, end = min(nums[0],M//len(nums)), nums[-1]
+
+    while start <= end:
+        mid = (start + end) // 2
+
+        total_val = sum(min(num, mid) for num in nums)
+
+        if total_val > M:
+            end = mid - 1
+        else:
+            start = mid + 1
+
+    return end
+
+
+def main():
+    N = int(input())
+    nums = list(map(int, input().split()))
+    M = int(input())
+
+    print(maximum_budget(nums, M))
+
+
+if __name__ == "__main__":
+    main()
