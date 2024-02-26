0f8fadcb859d11173834a76be7ad2e3636cae3fa
LEESUNGWOOK <40846999+gbk1414@users.noreply.github.com>
2024-02-26 19:49:45 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 120 ms, Memory: 33212 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -1,22 +1,29 @@
-from collections import deque
+import heapq as hq
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def get_least_coin(cave, N):
+def dijkstra(cave, N):
     min_cost = [[float('inf')] * N for _ in range(N)]
-    min_cost[N-1][N-1] = cave[N-1][N-1]
-    queue = deque([(N-1, N-1)])
+    min_cost[0][0] = cave[0][0]
+    heap = [(cave[0][0], 0, 0)]  # (비용, x좌표, y좌표)를 저장하는 최소 힙
 
-    while queue:
-        x, y = queue.popleft()
+    while heap:
+        cost, x, y = hq.heappop(heap)
+
+        if min_cost[x][y] < cost:  # 이미 더 작은 비용으로 갱신된 경우
+            continue
+
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
-            if 0 <= nx < N and 0 <= ny < N and min_cost[nx][ny] > min_cost[x][y] + cave[nx][ny]:
-                min_cost[nx][ny] = min_cost[x][y] + cave[nx][ny]
-                queue.append((nx, ny))
 
-    return min_cost[0][0]
+            if 0 <= nx < N and 0 <= ny < N:
+                new_cost = cost + cave[nx][ny]
+                if new_cost < min_cost[nx][ny]:
+                    min_cost[nx][ny] = new_cost
+                    hq.heappush(heap, (new_cost, nx, ny))
+
+    return min_cost[N-1][N-1]
 
 def main():
     cnt = 0
@@ -33,7 +40,7 @@ def main():
             row = list(map(int, input().split()))
             cave.append(row)
 
-        print("Problem {}:".format(cnt), get_least_coin(cave, N))
+        print("Problem {}:".format(cnt), dijkstra(cave, N))
 
 if __name__ == "__main__":
     main()
