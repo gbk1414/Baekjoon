1f01b960253eb85edd3113cf07be2afc20f6bcb9
LEESUNGWOOK <40846999+gbk1414@users.noreply.github.com>
2024-03-22 09:58:25 +0900

    [Gold I] Title: K번째 수, Time: 680 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.py
@@ -0,0 +1,22 @@
+def main():
+    N = int(input())
+    K = int(input())
+    
+    start, end = 1, K
+
+    while start <= end:
+        mid = (start + end) // 2
+        
+        temp = 0
+        for i in range(1, N + 1):
+            temp += min(mid // i, N)
+        if temp >= K:
+            answer = mid
+            end = mid - 1
+        else:
+            start = mid + 1
+    
+    print(answer)
+
+if __name__ == "__main__":
+    main()
