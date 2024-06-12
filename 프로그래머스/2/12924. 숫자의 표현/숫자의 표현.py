2cc4ef53c168fff4a4d1749d87098fa8bb904919
LEESUNGWOOK <40846999+gbk1414@users.noreply.github.com>
2024-06-12 18:31:53 +0900

    [level 2] Title: 숫자의 표현, Time: 0.20 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,13 +1,2 @@
 def solution(n):
-    answer = 0
-    i = 1
-    while i <= n:
-        sum = 0
-        j = i
-        while sum < n:
-            sum += j
-            j += 1
-        if sum == n:
-            answer += 1
-        i += 1
-    return answer
+    return len([i for i in range(1,n+1,2) if n%i == 0])
